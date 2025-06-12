a18c541580653c13a12b2606cf609c810d2a3ec8
alejandrobrambila <124195011+alejandrobrambila@users.noreply.github.com>
2025-06-12 12:49:31 -0400

    add survival data. edit script to show survival through 2025

--- SurvivalRates.R
+++ SurvivalRates.R
@@ -1,10 +1,21 @@
 #Willow Survival Rates
-#> SurvivalRates<- read_csv("SurvivalRates.csv")
+library(tidyverse)
+
+SurvivalRates<- read_csv("SurvivalRates.csv")%>%
+  mutate(season=paste(Season, Year, sep="_"))%>%
+  mutate(season=factor(season, levels = c("Fall_2023","Spring_2024","Summer_2024","Fall_2024","Spring_2025")))
+
 
 ggplot(
   data= SurvivalRates,
-  mapping= aes(x=Species, y= PercentSurvived, color= Tube))+
-  geom_jitter(width=.15)+
-  labs(y="Percent Survived", color="Legend")
+  mapping= aes(x=season, y= ((Count)), color= Species, shape=Tube))+
+  geom_jitter(width=.15) +
+  labs(y="Survivors", color="Legend")+ facet_wrap(~PlantingType)
 
-  
+ß
+
+ggplot(
+  data= SurvivalRates,
+  mapping= aes(x=season, y= (1/(Count/80)), color= Species, shape=Tube))+
+  geom_jitter(width=.15)+scale_y_log10()+
+  labs(y="Row Spacing (in ft)", color="Legend")+ facet_wrap(~PlantingType)
